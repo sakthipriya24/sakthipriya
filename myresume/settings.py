5dd60b9377860662cc4d52d19c0150ca6762cf27
SAKTHI PRIYA C <53993396+sakthipriya24@users.noreply.github.com>
2020-11-08 13:22:18 +0530

    Update settings.py

--- myresume/settings.py
+++ myresume/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*+j8v8-4txj7mk#=$#3a$!oshjpo%#a^kq#ullci7*ng4nq-z#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sakthipriya.herokuapp.com/']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
