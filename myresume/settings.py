75e75bed30b9311e69236056220825938d7b9d82
SAKTHI PRIYA C <53993396+sakthipriya24@users.noreply.github.com>
2020-12-05 19:57:18 +0530

    Update settings.py

--- myresume/settings.py
+++ myresume/settings.py
@@ -121,4 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/images/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
